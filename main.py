b462f17dac6904291c5868cd5b6eba7778c79ada
AyaanM <ayaan.merchant08@gmail.com>
2024-06-06 15:51:59 +0500

    check if file exists

---
+++ main.py
@@ -0,0 +1,23 @@
+'''
+title: sort article data
+author: Ayaan Merchant (ayaan.merchant08@gmail.com)
+date-created: 2024-06-06
+'''
+
+import sqlite3
+from pathlib import Path
+import pandas as pd
+
+def checkFile():
+    # Take file name input check if exists
+    fileName = input("File Name: ")
+    print("\n")
+    fileExists = False
+
+    while not fileExists:
+        my_file = Path(fileName)
+        if my_file.is_file():
+            fileExists = True
+        
+
+    return fileName
