9d3acc1d00942ee82ca032347336ca4df25977d3
AyaanM <ayaan.merchant08@gmail.com>
2024-06-06 15:53:07 +0500

    add info about program

--- main.py
+++ main.py
@@ -2,6 +2,13 @@
 title: sort article data
 author: Ayaan Merchant (ayaan.merchant08@gmail.com)
 date-created: 2024-06-06
+notes:
+    This program assumes the given CSV file was exported from Zoterro, has not yet been cleaned or sanitized in any way 
+    and is in the table format: 
+    ["Key","Item Type","Publication Year","Author","Title","Publication Title","ISBN","ISSN","DOI","Url","Abstract Note","Date","Date Added","Date Modified","Access Date","Pages","Num Pages","Issue","Volume","Number Of Volumes","Journal Abbreviation","Short Title","Series","Series Number","Series Text","Series Title","Publisher","Place","Language","Rights","Type","Archive","Archive Location","Library Catalog","Call Number","Extra","Notes","File Attachments","Link Attachments","Manual Tags","Automatic Tags","Editor","Series Editor","Translator","Contributor","Attorney Agent","Book Author","Cast Member","Commenter","Composer","Cosponsor","Counsel","Interviewer","Producer","Recipient","Reviewed Author","Scriptwriter","Words By","Guest","Number","Edition","Running Time","Scale","Medium","Artwork Size","Filing Date","Application Number","Assignee","Issuing Authority","Country","Meeting Name","Conference Name","Court","References","Reporter","Legal Status","Priority Numbers","Programming Language","Version","System","Code","Code Number","Section","Session","Committee","History","Legislative Body"]
+
+    This program will remove unnessesary types and only store:
+    [Item Type, Publication Year, Author(s), Title, Publication Title, DOI, URL, Abstract Note, Date Added, Pages, Issue, Volume, Short Title, Library Catologue, Link Attachments]
 '''
 
 import sqlite3
